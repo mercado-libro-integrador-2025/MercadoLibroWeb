89505f1e5bb629f3c950bdfda22ca472e879ec71
nahir1009 <nahir1009@gmail.com>
2025-05-13 01:14:42 -0300

    agrego cambios navbar mejoras

--- Frontend/src/app/guard/auth.guard.ts
+++ Frontend/src/app/guard/auth.guard.ts
@@ -13,12 +13,13 @@ export class AuthGuard {
     const cliente = this.loginService.obtenerClienteLogueado();
     console.log('Cliente logueado en canActivate:', cliente);
 
-    // Verificamos si el cliente tiene token y si el token no ha expirado
+    //  se verifica  si el cliente tiene token y si el token no ha expirado
     if (cliente && cliente.access && !this.loginService.isAccessTokenExpired()) {
       console.log('Acceso permitido');
       return true;
     } else {
       console.warn('Acceso denegado. Redirigiendo a /inicio');
+      alert('inicia sesión o registrate para acceder a esta página.');
       // Redirigir al usuario si el token no está presente o ha expirado
       this.router.navigate(['/inicio']);
       return false;
