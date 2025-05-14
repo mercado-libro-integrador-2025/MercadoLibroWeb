4a6d2bf46599947e9863335e2c90a25474802e88
Ivette <nobiltaivette@gmail.com>
2025-05-13 23:23:43 -0300

    transformo el listado de libros en un grid

--- Frontend/src/app/pages/nuestraseleccion/nuestraseleccion.component.ts
+++ Frontend/src/app/pages/nuestraseleccion/nuestraseleccion.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NuestraseleccionCarruselComponent } from './nuestraseleccion-carrusel/nuestraseleccion-carrusel.component';
 import { ProductoComponent } from './producto/producto.component';
 import { CarritoComponent } from './carrito/carrito.component';
 import { CommonModule } from '@angular/common';
@@ -7,7 +6,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nuestraseleccion',
-  imports: [CommonModule, RouterModule, NuestraseleccionCarruselComponent, ProductoComponent, CarritoComponent],
+  imports: [CommonModule, RouterModule, ProductoComponent, CarritoComponent],
   templateUrl: './nuestraseleccion.component.html',
   styleUrls: ['./nuestraseleccion.component.css'],
   standalone: true,
