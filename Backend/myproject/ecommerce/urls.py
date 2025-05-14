dde3cf745d035ae9f66181dd58213838c74182df
Ivette <nobiltaivette@gmail.com>
2025-05-14 19:40:31 -0300

    merge conflicts

--- Backend/myproject/ecommerce/urls.py
+++ Backend/myproject/ecommerce/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import LoginView, LogoutView, SignupView, UserViewSet, CategoriaViewSet, AutorViewSet, LibroViewSet, \
-<<<<<<< HEAD
 ItemCarritoViewSet, PedidoViewSet, DireccionViewSet, MetodoPagoViewSet, ReseñaViewSet, ContactoViewSet, confirmar_pedido, crear_preferencia
 from . import views
-=======
-ItemCarritoViewSet, PedidoViewSet, DireccionViewSet, MetodoPagoViewSet, ReseñaViewSet, ContactoViewSet
->>>>>>> e76d6427f9ee176a7ae747983d0cee56ed908870
 
 router = routers.DefaultRouter()
 router.register(r'usuarios', UserViewSet)
@@ -26,12 +22,9 @@ urlpatterns = [
     path('auth/login/', LoginView.as_view(), name='auth_login'),
     path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
     path('auth/signup/', SignupView.as_view(), name='auth_signup'),
-<<<<<<< HEAD
     path('checkout/crear-preferencia/', crear_preferencia, name='crear_preferencia'),
     path('api/confirmar-pedido/', confirmar_pedido, name='confirmar_pedido'),
     path('pago/success', views.pago_success, name='pago_success'),
     path('pago/pending', views.pago_pending, name='pago_pending'),
     path('pago/failure', views.pago_failure, name='pago_failure'),
-=======
->>>>>>> e76d6427f9ee176a7ae747983d0cee56ed908870
 ]
