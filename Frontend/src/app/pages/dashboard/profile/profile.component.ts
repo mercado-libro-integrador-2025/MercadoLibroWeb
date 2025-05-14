592f1093e6d61f85247a400ba722b27e16f031f8
Ivette <nobiltaivette@gmail.com>
2025-05-14 20:36:33 -0300

    direccion seralizer

--- Frontend/src/app/pages/dashboard/profile/profile.component.ts
+++ Frontend/src/app/pages/dashboard/profile/profile.component.ts
@@ -41,7 +41,7 @@ export class ProfileComponent implements OnInit {
   obtenerDireccionesCliente(): void {
   this.loginService.obtenerDirecciones().subscribe({
     next: (data) => {
-      console.log("Direcciones obtenidas:", data); // <== VERIFICAR
+      console.log("Direcciones obtenidas:", data);
       this.direcciones = data;
     },
     error: (error) => console.error('Error al obtener direcciones:', error)
