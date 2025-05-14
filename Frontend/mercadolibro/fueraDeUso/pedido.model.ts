f77466c1467d9a7cbbee8c9c2d35ac0e31d73c39
Ivette <nobiltaivette@gmail.com>
2025-05-13 21:15:16 -0300

    .

--- Frontend/mercadolibro/fueraDeUso/pedido.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Pedido {
-    usuario_cliente: number;
-    estado_pedido: string;
-    fecha_pedido?: string;
-    direccion_envio: number;
-    forma_envio: number;
-    forma_pago: number;
-}
