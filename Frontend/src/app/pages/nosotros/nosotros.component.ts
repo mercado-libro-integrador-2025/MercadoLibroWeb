0e0d7ac3aaaa3cd020c099fc6ee03f3fb889de06
rominarg <rominagnavarrete@gmail.com>
2025-05-28 03:28:36 -0300

    Agrego quienes somos: mision, vision, valores - responsive

--- Frontend/src/app/pages/nosotros/nosotros.component.ts
+++ Frontend/src/app/pages/nosotros/nosotros.component.ts
@@ -1,12 +1,12 @@
 
-import { NgForOf, NgStyle } from '@angular/common';
+import { CommonModule, NgFor, NgForOf, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-quienes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.css'
 })
@@ -20,18 +20,27 @@ export class QuienesComponent {
   historiaTexto = `MercadoLibro nació como una iniciativa para recuperar el valor de los libros físicos olvidados o almacenados sin uso. Lo que comenzó como un proyecto de estudiantes se convirtió en una plataforma que impulsa el acceso a la lectura a través del intercambio responsable, fortaleciendo una economía circular del conocimiento.`;
 
   historia = `MercadoLibro nació como una iniciativa para recuperar el valor de los libros físicos olvidados o almacenados sin uso...`;
+  
+  misionTitulo = 'Nuestra Misión';
+  misionTexto = 'Promover el acceso a la lectura mediante la revalorización e intercambio de libros físicos, facilitando una alternativa sostenible, colaborativa y económica que conecte a lectores de todas las edades y contextos.';
+  visionTitulo = 'Nuestra Visión';
+  visionTexto = 'Ser la plataforma líder en Latinoamérica para el intercambio responsable de libros físicos, fortaleciendo una comunidad lectora activa y generando impacto social, cultural y ambiental positivo a través de una economía circular del conocimiento.';
+  
   valores = [
     {
       titulo: 'Revalorización del libro físico',
-      texto: 'Creemos en el valor cultural y afectivo de los libros impresos, dándoles una segunda vida útil.'
+      descripcion: 'Creemos en el valor cultural y afectivo de los libros impresos, dándoles una segunda vida útil.',
+      icono: '✔'
     },
     {
       titulo: 'Acceso equitativo a la lectura',
-      texto: 'Apostamos a facilitar el acceso al conocimiento sin depender del costo de los libros nuevos.'
+      descripcion: 'Apostamos a facilitar el acceso al conocimiento sin depender del costo de los libros nuevos.',
+      icono: '✔'
     },
     {
       titulo: 'Comunidad colaborativa',
-      texto: 'Construimos redes solidarias entre lectores que comparten, intercambian y se apoyan mutuamente.'
+      descripcion: 'Construimos redes solidarias entre lectores que comparten, intercambian y se apoyan mutuamente.',
+      icono: '✔'
     }
   ];
 }
